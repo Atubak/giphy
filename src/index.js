992dcc25cfa955606dc18fa0399484d53ed5c954
Jona <mr-jona@hotmail.com>
2022-06-22 22:03:41 +0200

    add more logic

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import './style.css';
 
+let fetchCounter = 0;
+
 async function fetchGif(searchTerm = 'lol') {
   const url = `https://api.giphy.com/v1/gifs/translate?api_key=rpbWQ0CSEoOJ4QMjk94L3bhXndUqX44E&s=${searchTerm}`;
 
   const response = await fetch(url, { mode: 'cors' });
   const gifData = await response.json();
-  img.src = gifData.data.images.original.url;
+  fetchCounter += 1;
+
+  const newImg = document.createElement('img');
+  newImg.id = `img-${fetchCounter}`;
+  newImg.src = gifData.data.images.original.url;
+
+  const newDiv = document.createElement('div');
+  newDiv.id = `div-${fetchCounter}`;
+  newDiv.classList.add('imgDiv');
+
+  newDiv.appendChild(newImg);
+  document.getElementById('div').appendChild(newDiv);
 }
 
 function inputGif(e) {
@@ -17,8 +30,8 @@ function inputGif(e) {
 }
 
 function render() {
-  const img = document.createElement('img');
-  img.id = 'img';
+  const div = document.createElement('div');
+  div.id = 'div';
 
   const form = document.createElement('form');
   form.id = 'form';
@@ -35,7 +48,7 @@ function render() {
   form.appendChild(document.createElement('br'));
   form.appendChild(btn);
 
-  document.body.appendChild(img);
+  document.body.appendChild(div);
   document.body.appendChild(document.createElement('br'));
   document.body.appendChild(form);
 
